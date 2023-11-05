014f88e4f68af77a005320fcacf5c4bfb9fc91fd
Chandler Abril <abrilchandler1@gmail.com>
2023-11-04 21:03:54 -0700

    not too bad

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+/* document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector('form')
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-   buildToDo(e.target.to_do.value) // your code here
+   buildToDo(e.target.to_do.value) 
+   form.reset()// your code here
 })
 })
 
@@ -19,7 +20,32 @@ function buildToDo(todo) {
 function handleDelete(e) {
   e.target.parentNode.remove()
 }
+*/
 
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target.new_task_description.value)
+    
+  })
+})
+
+function handleToDo(todo){
+  console.log(todo)
+  let p = document.createElement('p')
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x'
+  p.textContent = todo
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+  document.querySelector("form").reset()
+  console.log(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
 
 
 
